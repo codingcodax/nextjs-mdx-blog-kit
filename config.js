a1170f12da6bd477023542f9cb01239284ac1c70
Alexis Guzman <alexis12989x@gmail.com>
2021-07-27 14:16:53 -0500

    feat: create configuration file

---
+++ config.js
@@ -0,0 +1,14 @@
+const name = 'Alexis Guzman';
+const career = 'Front-end Developer';
+
+const config = {
+	name,
+	career,
+	seo: {
+		title: `${name} – ${career}`,
+		description:
+			'Freelance font-end web developer focused on building scalable, maintainable and accessible solutions on the web.',
+	},
+};
+
+export default config;
