22e86b2eb47e5aca609292c45fda605e3190fa7a
Alexis Guzman <alexis12989x@gmail.com>
2021-07-27 16:28:29 -0500

    feat(seo): add default seo and import layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from '../next-seo.config';
 
 import '@fontsource/poppins/400.css';
-
 import theme from '@/styles/theme';
+
+import Layout from '@/components/layout';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<ChakraProvider resetCSS theme={theme}>
-			<Component {...pageProps} />
+			<Layout>
+				<DefaultSeo {...SEO} />
+				<Component {...pageProps} />
+			</Layout>
 		</ChakraProvider>
 	);
 };
