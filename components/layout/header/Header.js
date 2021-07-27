34a690356841294ed785b71a9dd0008c9e2413a3
Alexis Guzman <alexis12989x@gmail.com>
2021-07-27 16:24:03 -0500

    feat(layout): set up header component

---
+++ components/layout/header/Header.js
@@ -0,0 +1,28 @@
+import { Flex } from '@chakra-ui/react';
+
+import Avatar from './Avatar';
+import Nav from './Nav';
+
+const Header = () => {
+	return (
+		<Flex
+			as='header'
+			py={{ base: 2, sm: 4 }}
+			px={{ base: 2.5, sm: 5 }}
+			w={{ base: 'calc(100vw - 1rem)', sm: 'calc(100vw - 4rem)' }}
+			maxW='calc(1200px - 64px)'
+			bg='linear-gradient(90.89deg, rgba(0, 0, 0, 0.6) 25.56%, rgba(0, 0, 0, 0.3) 62.83%);'
+			boxShadow='0px 4px 24px -1px rgba(0, 0, 0, 0.2);'
+			backdropFilter='blur(40px)'
+			borderRadius='20px'
+			justify='space-between'
+			position='fixed'
+			border='1px solid rgba(0, 0, 0, 0.8)'
+		>
+			<Avatar />
+			<Nav />
+		</Flex>
+	);
+};
+
+export default Header;
