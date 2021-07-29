5d6d5b5471c48eb4050058d32e73a990e9d6bc4c
Alexis Guzman <alexis12989x@gmail.com>
2021-07-29 17:52:49 -0500

    feat: set up footer component

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,8 +1,50 @@
+import { Button, Flex, Text, useColorMode } from '@chakra-ui/react';
+
+import useColorModeValues from '@/hooks/useColorModeValues';
+
 const Footer = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const {
+		headingSecondaryColor,
+		glassBgOne,
+		glassBgTwo,
+		glassBoxShadow,
+		glassBorder,
+	} = useColorModeValues();
+
 	return (
-		<div>
-			<p>Footer Component</p>
-		</div>
+		<Flex
+			py={{ base: 2, sm: 4 }}
+			px={{ base: 2.5, sm: 5 }}
+			mt={48}
+			align='center'
+			justify='space-between'
+			bg={`linear-gradient(90.89deg, ${glassBgOne} 25.56%, ${glassBgTwo} 62.83%);`}
+			boxShadow={`0px 4px 24px -1px ${glassBoxShadow};`}
+			backdropFilter='blur(40px)'
+			borderRadius='20px'
+			border={`1px solid ${glassBorder}`}
+		>
+			<Text
+				fontSize='sm'
+				textTransform='capitalize'
+				color={headingSecondaryColor}
+			>
+				Made with love by ❤️ Codax
+			</Text>
+			<Button onClick={toggleColorMode}>
+				{colorMode === 'light'
+					? 'Toggle dark mode'
+					: 'Toggle light Mode'}
+			</Button>
+			<Text
+				fontSize='sm'
+				textTransform='capitalize'
+				color={headingSecondaryColor}
+			>
+				Copyright © 2021 Alexis Guzman
+			</Text>
+		</Flex>
 	);
 };
 
