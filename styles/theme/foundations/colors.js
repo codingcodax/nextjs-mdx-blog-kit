c21eff263131d7522ad0a69a7fc5104ffc44c344
Alexis Guzman <alexis12989x@gmail.com>
2021-07-29 17:37:28 -0500

    feat(theme): add neutrals colors

---
+++ styles/theme/foundations/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+	neutral: {
+		100: '#fefefe',
+		200: '#f1f1f1',
+		300: '#eaeaea',
+		400: '#d1d1d1',
+		500: '#4d4d4d',
+		600: '#212121',
+		700: '#101010',
+		800: '#0b0b0b',
+	},
+};
+
+export default colors;
