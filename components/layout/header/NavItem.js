0c47908cc7ba3fc948096110e05267601e8a13f0
Alexis Guzman <alexis12989x@gmail.com>
2021-07-29 17:46:18 -0500

    feat: replace rgba values with values of use color mode values
    set color, text transform, font size and font weight

--- components/layout/header/NavItem.js
+++ components/layout/header/NavItem.js
@@ -1,11 +1,22 @@
 import { VStack } from '@chakra-ui/react';
 
 import Link from '@/components/Link';
+import useColorModeValues from '@/hooks/useColorModeValues';
 
 const NavItem = ({ href, children }) => {
+	const { headingSecondaryColor } = useColorModeValues();
+
 	return (
 		<VStack as='li' listStyleType='none'>
-			<Link href={href}>{children}</Link>
+			<Link
+				href={href}
+				color={headingSecondaryColor}
+				textTransform='uppercase'
+				fontSize='sm'
+				fontWeight='500'
+			>
+				{children}
+			</Link>
 		</VStack>
 	);
 };
