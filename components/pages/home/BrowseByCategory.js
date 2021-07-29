6900e9d70fc74a9e4d22e117459e5bf44af06e91
Alexis Guzman <alexis12989x@gmail.com>
2021-07-29 17:51:11 -0500

    feat: create browse by category component

---
+++ components/pages/home/BrowseByCategory.js
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/react';
+
+const BrowseByCategory = () => {
+	return (
+		<Box>
+			<Text>Browse By Category</Text>
+		</Box>
+	);
+};
+
+export default BrowseByCategory;
