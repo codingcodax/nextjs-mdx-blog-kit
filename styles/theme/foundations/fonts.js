f353c6907f66e5cbe3d92bb58f31c417511c1164
Alexis Guzman <alexis12989x@gmail.com>
2021-07-29 10:37:19 -0500

    feat(theme): set poppins font to heading

--- styles/theme/foundations/fonts.js
+++ styles/theme/foundations/fonts.js
@@ -1,4 +1,5 @@
 const fonts = {
+	heading: 'Poppins',
 	body: 'Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
 };
 
