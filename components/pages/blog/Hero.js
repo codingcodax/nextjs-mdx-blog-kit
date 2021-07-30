3b2d4cb8176d6a754fff6db17fbd7ddfc49c34e2
Alexis Guzman <alexis12989x@gmail.com>
2021-07-30 09:32:44 -0500

    feat: set color prop to heading primary color

--- components/pages/blog/Hero.js
+++ components/pages/blog/Hero.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { Center, Grid, Heading, useColorMode } from '@chakra-ui/react';
 
+import useColorModeValues from '@/hooks/useColorModeValues';
+
 import config from '../../../config';
 
 import CodePencil from '../../../public/images/3d-code-pencil.webp';
@@ -9,11 +11,18 @@ const { name, career } = config;
 
 const Hero = () => {
 	const { colorMode } = useColorMode();
+	const { headingPrimaryColor } = useColorModeValues();
 
 	return (
 		<Grid templateColumns='1fr 1fr' columnGap={6}>
 			<Center>
-				<Heading as='h1' w='100%' fontSize={40} lineHeight={1.3}>
+				<Heading
+					as='h1'
+					w='100%'
+					color={headingPrimaryColor}
+					fontSize={40}
+					lineHeight={1.3}
+				>
 					Hi there,
 					<br />
 					{`I'm ${name} a`}
