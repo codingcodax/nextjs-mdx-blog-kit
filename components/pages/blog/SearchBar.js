baf5c9b4f5e2d597166922e999a734204a4b7af2
Alexis Guzman <alexis12989x@gmail.com>
2021-07-30 09:33:38 -0500

    feat: set up search bar component

--- components/pages/blog/SearchBar.js
+++ components/pages/blog/SearchBar.js
@@ -1,10 +1,32 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Input, InputGroup } from '@chakra-ui/react';
 
-const SearchBar = () => {
+import useColorModeValues from '@/hooks/useColorModeValues';
+
+const SearchBar = ({ value, handleChange }) => {
+	const {
+		textColor,
+		grayColor,
+		glassBgOne,
+		glassBgTwo,
+		glassBoxShadow,
+		glassBorder,
+	} = useColorModeValues();
+
 	return (
-		<Box>
-			<Text>Search Articles</Text>
-		</Box>
+		<InputGroup mt={5} maxW='container.sm'>
+			<Input
+				value={value}
+				onChange={handleChange}
+				color={textColor}
+				_placeholder={{ color: grayColor }}
+				bg={`linear-gradient(90.89deg, ${glassBgOne} 25.56%, ${glassBgTwo} 62.83%);`}
+				boxShadow={`0px 4px 24px -1px ${glassBoxShadow};`}
+				backdropFilter='blur(40px)'
+				borderRadius='20px'
+				border={`1px solid ${glassBorder}`}
+				placeholder='Search articles'
+			/>
+		</InputGroup>
 	);
 };
 
