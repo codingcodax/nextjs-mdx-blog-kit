e5d0b9dfb98ec5548786995851f1c9404608c7ca
Alexis Guzman <alexis12989x@gmail.com>
2021-07-30 17:10:09 -0500

    feat: create categories file

---
+++ data/categories.js
@@ -0,0 +1,12 @@
+import HTMLIcon from '../public/images/icons/html.min.svg';
+import CSSIcon from '../public/images/icons/css.min.svg';
+import JSIcon from '../public/images/icons/javascript.min.svg';
+// import ReactIcon from '../public/images/icons/react.min.svg';
+
+const categories = [
+	{ name: 'html', icon: HTMLIcon },
+	{ name: 'css', icon: CSSIcon },
+	{ name: 'javascript', icon: JSIcon },
+];
+
+export default categories;
