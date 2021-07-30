af6969bd7ef2df8064fb879e4b5d0dfac67d9234
Alexis Guzman <alexis12989x@gmail.com>
2021-07-30 09:36:48 -0500

    feat(page): set up blog page component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,48 @@
-const Blog = () => {
+import { useState } from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+import useColorModeValues from '@/hooks/useColorModeValues';
+
+import { getPostsFrontMatter } from '@/utils/mdx';
+
+import SearchBar from '@/components/pages/blog/SearchBar';
+import BlogPosts from '@/components/BlogPosts';
+
+export const getStaticProps = async () => {
+	const posts = await getPostsFrontMatter();
+
+	return { props: { posts } };
+};
+
+const Blog = ({ posts }) => {
+	const { headingPrimaryColor } = useColorModeValues();
+	const [searchValue, setSearchValue] = useState('');
+	const filteredBlogPosts = posts
+		.sort(
+			(a, b) =>
+				Number(new Date(b.publishedAt)) -
+				Number(new Date(a.publishedAt))
+		)
+		.filter((frontMatter) =>
+			frontMatter.title.toLowerCase().includes(searchValue.toLowerCase())
+		);
+
 	return (
-		<section>
-			<p>Blog Page Component</p>
-		</section>
+		<Box mt={48} mx='auto' w='100%' maxW='1000px'>
+			<Heading as='h1' color={headingPrimaryColor} fontSize={40}>
+				Blog Posts
+			</Heading>
+
+			<SearchBar
+				value={searchValue}
+				handleChange={(e) => setSearchValue(e.target.value)}
+			/>
+
+			<BlogPosts
+				title={searchValue.length > 0 ? 'Results' : 'Latest'}
+				posts={filteredBlogPosts}
+			/>
+		</Box>
 	);
 };
 
