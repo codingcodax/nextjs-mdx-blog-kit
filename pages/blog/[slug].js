5dc0bc8853fcb1798d588a514b185097e3964d0c
Alexis Guzman <alexis12989x@gmail.com>
2021-08-02 16:41:41 -0500

    feat(page): set up blog post page component

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,51 @@
-const BlogPost = () => {
+import { MDXRemote } from 'next-mdx-remote';
+
+import { getPostBySlug, getPosts } from '@/utils/mdx';
+import { Box, Heading } from '@chakra-ui/react';
+
+import Image from '@/components/pages/blog/Image';
+import Author from '@/components/pages/blog/Author';
+import MDXComponents from '@/components/MDXComponents';
+
+export const getStaticPaths = async () => {
+	const posts = await getPosts();
+	const paths = posts.map((p) => ({
+		params: { slug: p.replace(/\.mdx/, '') },
+	}));
+
+	return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+	const { mdxSource, frontMatter } = await getPostBySlug(slug);
+
+	return { props: { mdxSource, frontMatter, slug } };
+};
+
+const BlogPost = ({ mdxSource, frontMatter, slug }) => {
 	return (
-		<div>
-			<p>Blog Post page component</p>
-		</div>
+		<Box mt={48} mx='auto' w='100%' maxW='container.sm'>
+			<Image
+				src={frontMatter.image}
+				blurDataURL={frontMatter.imageBlur}
+				alt={frontMatter.title}
+				height='400'
+				width='800'
+			/>
+
+			<Heading as='h1' mt={2} fontSize={40}>
+				{frontMatter.title}
+			</Heading>
+
+			<Author
+				publishedAt={frontMatter.publishedAt}
+				readingTime={frontMatter.readingTime}
+			/>
+
+			<Box as='section' mt={20}>
+				<MDXRemote {...mdxSource} components={MDXComponents} />
+			</Box>
+		</Box>
 	);
 };
 
