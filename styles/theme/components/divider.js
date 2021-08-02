aba0112a079d51e3ba94386aed39769d5b35bf9c
Alexis Guzman <alexis12989x@gmail.com>
2021-08-02 16:51:11 -0500

    feat: set up divider component and import it

---
+++ styles/theme/components/divider.js
@@ -0,0 +1,7 @@
+const Divider = {
+	baseStyle: ({ colorMode }) => ({
+		borderColor: colorMode === 'light' ? 'neutral.100' : 'neutral.600',
+	}),
+};
+
+export default Divider;
--- styles/theme/index.js
+++ styles/theme/index.js
@@ -13,13 +13,14 @@ import colors from './foundations/colors';
 // Component style overrides
 import Heading from './components/heading';
 import Code from './components/code';
+import Divider from './components/divider';
 
 const overrides = {
 	config,
 	styles,
 	fonts,
 	colors,
-	components: { Heading, Code },
+	components: { Heading, Code, Divider },
 };
 
 const theme = extendTheme(overrides);
