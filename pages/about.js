e0d9f1c10a18ed16966731e1954a8dc1ea29384c
Alexis Guzman <alexis12989x@gmail.com>
2021-08-05 17:30:05 -0500

    feat(page): set up about page component

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,18 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+import AboutText from '@/components/pages/about/AboutText';
+import Contact from '@/components/pages/about/contact';
+
 const About = () => {
 	return (
-		<section>
-			<p>About Page Component</p>
-		</section>
+		<Box mt={48} mx='auto' w='100%' maxW='container.sm'>
+			<Heading as='h1' mt={2} fontSize={40}>
+				About
+			</Heading>
+			<AboutText />
+
+			<Contact />
+		</Box>
 	);
 };
 
