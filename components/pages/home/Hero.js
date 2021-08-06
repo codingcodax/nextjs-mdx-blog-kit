4a0cb81070c1e2740af8844261a0ed05d4dc6f7b
Alexis Guzman <alexis12989x@gmail.com>
2021-08-06 10:01:40 -0500

    feat(home): make hero component resposive

--- components/pages/home/Hero.js
+++ components/pages/home/Hero.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { Center, Grid, Heading, useColorMode } from '@chakra-ui/react';
 
+import useMediaQueries from '@/hooks/useMediaQueries';
+
 import config from '../../../config';
 
 import CodePencil from '../../../public/images/3d-code-pencil.webp';
@@ -9,9 +11,10 @@ const { name, career } = config;
 
 const Hero = () => {
 	const { colorMode } = useColorMode();
+	const { isMediumSize } = useMediaQueries();
 
 	return (
-		<Grid templateColumns='1fr 1fr' columnGap={6}>
+		<Grid templateColumns={{ base: '1fr', md: '1fr 1fr' }} columnGap={6}>
 			<Center>
 				<Heading as='h1' w='100%' fontSize={40} lineHeight={1.3}>
 					Hi there,
@@ -21,20 +24,22 @@ const Hero = () => {
 					{`${career}`}
 				</Heading>
 			</Center>
-			<Center
-				filter={
-					colorMode === 'light'
-						? null
-						: 'contrast(1.1) brightness(0.7) saturate(1.2)'
-				}
-			>
-				<Image
-					src={CodePencil}
-					height='704'
-					width='968'
-					alt='code pencil'
-				/>
-			</Center>
+			{isMediumSize && (
+				<Center
+					filter={
+						colorMode === 'light'
+							? null
+							: 'contrast(1.1) brightness(0.7) saturate(1.2)'
+					}
+				>
+					<Image
+						src={CodePencil}
+						height='704'
+						width='968'
+						alt='code pencil'
+					/>
+				</Center>
+			)}
 		</Grid>
 	);
 };
