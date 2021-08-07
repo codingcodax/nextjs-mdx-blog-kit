3dc46442ee9370dc83f4d2a170a195b65b0ac0ee
Alexis Guzman <alexis12989x@gmail.com>
2021-08-07 11:28:05 -0500

    feat: add limit of post and import see more component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { getPostsFrontMatter } from '@/utils/mdx';
 
 import Hero from '@/components/pages/home/Hero';
 import BlogPosts from '@/components/BlogPosts';
+import SeeMore from '@/components/pages/home/SeeMore';
 
 export const getStaticProps = async () => {
 	const posts = await getPostsFrontMatter();
@@ -12,15 +13,19 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ posts }) => {
-	const sortedBlogPosts = posts.sort(
-		(a, b) =>
-			Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
-	);
+	const sortedBlogPosts = posts
+		.sort(
+			(a, b) =>
+				Number(new Date(b.publishedAt)) -
+				Number(new Date(a.publishedAt))
+		)
+		.slice(0, 3);
 
 	return (
 		<Box mt={48} mx='auto' maxW='1000px'>
 			<Hero />
 			<BlogPosts title='Latest' posts={sortedBlogPosts} />
+			{sortedBlogPosts.length >= 3 && <SeeMore />}
 		</Box>
 	);
 };
