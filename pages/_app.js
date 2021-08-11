36cf3e2037aea775f84f88cf9f42dec282fcefd2
Alexis Guzman <alexis12989x@gmail.com>
2021-08-11 18:03:38 -0500

    feat: import head component

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,13 @@ import '@fontsource/poppins/500.css';
 import theme from '@/styles/theme';
 
 import Layout from '@/components/layout';
+import Head from '@/components/Head';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<Layout>
+				<Head />
 				<DefaultSeo {...SEO} />
 				<Component {...pageProps} />
 			</Layout>
