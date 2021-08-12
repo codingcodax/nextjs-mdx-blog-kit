251ff009092a66e516b4d59e68174566878e72b6
Alexis Guzman <alexis12989x@gmail.com>
2021-08-12 09:50:35 -0500

    chore(seo): add viewport and shortcut icon link tags

--- components/Head.js
+++ components/Head.js
@@ -3,27 +3,33 @@ import NextHead from 'next/head';
 const Head = () => {
 	return (
 		<NextHead>
+			<meta content='IE=edge' httpEquiv='X-UA-Compatible' />
+			<meta
+				content='width=device-width, initial-scale=1'
+				name='viewport'
+			/>
+			<link href='/favicons/favicon.ico' rel='shortcut icon' />
 			<link
 				rel='apple-touch-icon'
 				sizes='180x180'
-				href='/apple-touch-icon.png'
+				href='/favicons/apple-touch-icon.png'
 			/>
 			<link
 				rel='icon'
 				type='image/png'
 				sizes='32x32'
-				href='/favicon-32x32.png'
+				href='/favicons/favicon-32x32.png'
 			/>
 			<link
 				rel='icon'
 				type='image/png'
 				sizes='16x16'
-				href='/favicon-16x16.png'
+				href='/favicons/favicon-16x16.png'
 			/>
-			<link rel='manifest' href='/site.webmanifest' />
+			<link rel='manifest' href='/favicons/site.webmanifest' />
 			<link
 				rel='mask-icon'
-				href='/safari-pinned-tab.svg'
+				href='/favicons/safari-pinned-tab.svg'
 				color='#805ad5'
 			/>
 			<meta name='msapplication-TileColor' content='#805ad5' />
